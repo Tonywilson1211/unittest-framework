2548971112ba87891619f1806be5c2d80bbdcede
Tonywilson1211 <>
2023-05-09 21:09:31 +0000

    save

--- evens.py
+++ evens.py
@@ -1,26 +1,20 @@
 def even_number_of_evens(numbers):
     """
-    Should raise TypeError if a list is not passed into the function
+    Should Raise a TypeError if a list in not passed into the function
     error message: "A list was not passed into the function"
-    if numbers is empty return False
-    if the number of evens is odd, return False
-    if the number of evens is 0, return False
-    if the number of evens is even, return True
+    if the list is empty it will return False
+    if the number of even numbers is odd - return False
+    if the numner of even numbers is even - return True
     """
     if isinstance(numbers, list):
-        evens = 0
+        evens = sum([1 for n in numbers if n % 2 == 0])
 
-        for n in numbers:
-            if n % 2 == 0:
-                evens += 1
+        return True if evens and evens % 2 == 0 else False
 
-        if evens:
-            return evens % 2 == 0
-        else:
-            return False
     else:
-        raise TypeError("A list wass not passed into the function")
+        raise TypeError("A list was not passed into the function")
+    return None
 
 
-if __name__ == '__main__':
-    print(even_number_of_evens(5))
+if __name__ == "__main__":
+    print(even_number_of_evens([2, 1, 4]))
